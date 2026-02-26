049830c4c249764efbc55e223737a350a0367f2a
Davi-CGC <davi.9120@colegioprogresso.g12.br>
2026-02-26 07:59:13 -0300

    Add files via upload

---
+++ API.js
@@ -0,0 +1,29 @@
+const pesquisa = document.getElementById("pesquisa")
+const button = document.getElementById("button")
+const divResultado = document.getElementById("resultado")
+
+const cotarAcao = async () => {
+  const acao = pesquisa.value 
+  const response = await (await fetch(`https://pokeapi.co/api/v2/pokemon/${acao}`)).json()
+
+  const imagem = response.sprites.front_default
+  const nome = response.name
+  const altura = response.height
+  const peso = response.weight
+  const tipo = response.types[0].type.name
+  const id = response.id
+
+  divResultado.innerHTML = `<img src="${imagem}">
+  <p>ID: ${id}</p>
+  <p>Nome: ${nome}</p>
+  <p>Tipo: ${tipo}</p>
+  <p>Altura: ${altura}m</p>
+  <p>Peso: ${peso}Kg</p>`
+}
+
+button.addEventListener("click", cotarAcao)
+
+//await = freio para carregar
+//fetch = requisição
+//getElementById = busca no html
+//.json = conversor de promessas em js
